b24bb4ed7b4c085aea65680935f66cda54caa2f7
Kieran911 <arinzekieran@gmail.com>
2025-04-15 22:03:00 +0100

    Add body-parser middleware and update product form handling in app.js

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); // app could be any name of your choice
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+  next();
+});
+
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Add product</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from express</h1>');
+  res.send('<h1>Hello from Express</h1>');
 });
 
 // const server = http.createServer(app);
