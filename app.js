117ebf806f2d63928dfa6bf45449ad934bcfcaf9
Kieran911 <arinzekieran@gmail.com>
2025-04-15 21:13:17 +0100

    Update app.js to implement middleware for product addition and root route handling

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const http = require('http');
-
 const express = require('express');
 
-const app = express();
+const app = express(); // app could be any name of your choice
 
-const server = http.createServer(routes);
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Add product</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from express</h1>');
+});
+
+// const server = http.createServer(app);
 // const server = http.createServer(routes.handler);
+//server.listen(3000);
 
-server.listen(3000);
+app.listen(3000);
