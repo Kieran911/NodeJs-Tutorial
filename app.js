8248fba929d382b8b9a14de26f83d3420023922f
Kieran911 <arinzekieran@gmail.com>
2025-04-16 08:17:37 +0100

    Refactor routing structure by separating admin and shop routes; remove unused routes file

--- app.js
+++ app.js
@@ -3,29 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express(); // app could be any name of your choice
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  next();
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
 });
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>');
-});
-
-// const server = http.createServer(app);
-// const server = http.createServer(routes.handler);
-//server.listen(3000);
 
 app.listen(3000);
