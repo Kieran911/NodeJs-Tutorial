a1ef9bdf281a543c6015468b507a26228e319205
Kieran911 <arinzekieran@gmail.com>
2025-04-17 14:21:56 +0100

    Refactor admin routes to use consistent naming; add products array for product management

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const routes = express.Router();
 
 routes.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
