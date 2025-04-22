a9cae38f4c41a5064de69a6932c23d9334ac737e
Kieran911 <arinzekieran@gmail.com>
2025-04-22 06:59:51 +0100

    Refactor routing and controllers; replace inline logic with dedicated controller methods for add-product and shop routes, and implement centralized error handling for 404 responses.

--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,10 @@
 const path = require('path');
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const routes = express.Router();
 
-routes.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'The Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+routes.get('/', productsController.getProducts);
 
 module.exports = routes;
